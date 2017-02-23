aaa4cc90e3c044e666ccf250865e5821a5f33102
Kamil Sobecki <revannn5480@gmail.com>
2017-02-23 11:09:03 +0100

    Adding new recipe to db - works :)

--- FYM/src/AppBundle/Controller/RecipeController.php
+++ FYM/src/AppBundle/Controller/RecipeController.php
@@ -39,64 +39,94 @@ class RecipeController extends Controller
      */
     public function saveInDatabaseAction(Request $request)
     {
+        // create new object Recipe
         $recipe = new Recipe();
 
+        // create form and get data from that form
         $form = $this->createForm(new RecipeType(), $recipe);
         $form->handleRequest($request);
 
         // ADD VALIDATION TO FORM AND CHECK VALIDATION
+
+        // if form was submitted
         if($form->isSubmitted()) {
+
+            // create new object doctrine -> manager
             $em = $this->getDoctrine()->getManager();
 
+            //set recipe from form including type of ingredients and their quantity
+            $i = 0;
             foreach ($request->get('igredients') as $igredient) {
-
                 $quantity = new Quantity();
+
+                // find in database object equal id from igredients array[]
+                $igredient = $this->getDoctrine()->getRepository('AppBundle:Igredient')->find($igredient);
+
+                // set all attributes
                 $quantity->setIgredient($igredient);
+                $quantity->setQuantity($request->get('quantity')[$i]);
+                $quantity->setRecipe($recipe);
+//                $recipe->addIgredientQuantity($quantity);
+                // tell database about new quantity object
                 $em->persist($quantity);
-//                print_r($igredient);
-
-            // USUNALEM type hinting z setterow do igredients i quantity - co moze powodowac błedy
-            }
-            foreach ($request->get('quantity') as $quant) {
-
-                $quantity = new Quantity();
-                $quantity->setQuantity($quant);
-                $em->persist($quantity);
+                $i++;
 
             }
-//            print_r($request->get('igredients'));
-//            print_r($request->get('quantity'));
-//            die;
-            //
-//            $quantity->setRecipe($request->request->get(''));
-//            $quantity->setIgredient($request->request->get(''));
-//            $quantity->setQuantity($request->request->get())
 
-//            $em = $this->getDoctrine()->getManager();
+            // tell database about new recipe object and than save it and quantity object in database
             $em->persist($recipe);
-//            $em->persist($quantity);
             $em->flush();
 
+            // after add new recipe, show it
             return $this->redirectToRoute('app_recipe_show', ['id' => $recipe->getId()]);
         }
 
         return ['form' => $form->createView()];
     }
 
+    // method for show one recipe with get id
     /**
      * @Route("/{id}")
      * @Template()
      */
     public function showAction($id)
     {
+        // find in database recipe with get id
         $recipe = $this->getDoctrine()->getRepository('AppBundle:Recipe')->find($id);
 
+        // if recipe doesn't exist throw exception
         if(!$recipe) {
 
             throw $this->createNotFoundException('Recipe not found');
         }
 
+        // show recipe
         return ['recipe' => $recipe];
+    }
+
+    // method for show all recipes in database
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function showAllAction()
+    {
+        //find all recipes in database
+        $recipes = $this->getDoctrine()->getRepository('AppBundle:Recipe')->findAll();
+
+        // show all recipes as links(it is set in Twig)
+        return ['recipes' => $recipes];
+    }
+
+    // edit recipe with get id
+    /**
+     * @@Route("/{id}/edit")
+     * @Template("AppBundle:Recipe:create.html.twig")
+     */
+    public function editAction($id)
+    {
+        // find in database recipe with get id
+        $recipe = $this->getDoctrine()->getRepository('AppBundle:Recipe')->find($id);
     }
 
 }
